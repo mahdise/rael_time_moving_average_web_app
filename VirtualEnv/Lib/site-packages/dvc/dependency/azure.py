97e598aaef6f33dbd622a4dc8084bf88fd5c2a34
Rakibul Islam <irakib.rana@gmail.com>
2021-01-14 00:54:26 +0100

    VirtualEvn folder deletd

--- VirtualEnv/Lib/site-packages/dvc/dependency/azure.py
+++
@@ -1,8 +0,0 @@
-from dvc.dependency.base import BaseDependency
-from dvc.output.base import BaseOutput
-
-from ..tree.azure import AzureTree
-
-
-class AzureDependency(BaseDependency, BaseOutput):
-    TREE_CLS = AzureTree
--- VirtualEnv/Lib/site-packages/dvc/output/ssh.py
+++
@@ -1,7 +0,0 @@
-from dvc.output.base import BaseOutput
-
-from ..tree.ssh import SSHTree
-
-
-class SSHOutput(BaseOutput):
-    TREE_CLS = SSHTree
