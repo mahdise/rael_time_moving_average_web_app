b03919d77baeab5a8447a4461f9edd7a01a149c2
mahdi <mahdislam12@gmail.com>
2021-01-13 21:47:11 +0100

    add project structure

---
+++ calculate_sma.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+#seam
+def read_and_procees_data():
+    pass
+
+#mahdi
+def resample_data_based_on_input(interval_time=1, data_frame=None):
+    data_frame = {'time': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                  'price': [290, 260, 288, 300, 310, 303, 329, 340, 316, 330, 308, 310]}
+
+    df = pd.DataFrame(data_frame)
+    resample_data_frame = None
+    return resample_data_frame
+    pass
+
+#rana bai
+def calculate_sma(data_from_resample):
+    moving_avg_data_frame = None
+    return moving_avg_data_frame
+
+# if __name__ == '__main__':
+#     procees_data = read_and_procees_data
--- sma_real_time.py
+++ sma_real_time.py
@@ -5,6 +5,6 @@ product = {'month' : [1,2,3,4,5,6,7,8,9,10,11,12],'demand':[290,260,288,300,310,
 
 df = pd.DataFrame(product)
 for i in range(0,df.shape[0]-2):
-    df.loc[df.index[i+2],'SMA_3'] = np.round(((df.iloc[i,1]+ df.iloc[i+1,1] +df.iloc[i+2,1])/3),1)
+    df.loc[df.index[i+1],'SMA_3'] = np.round(((df.iloc[i,1]+ df.iloc[i+1,1] )/2),1)
 
 print(df)
